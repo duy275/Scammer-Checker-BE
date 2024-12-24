ca36fb3f3962f699958e1f69190a76d604898b13
Bui Hoang Duy <hoangduygala@gmail.com>
2024-12-25 00:46:00 +0700

    BE deploy

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "duycan123",
   database: "scammerchecker",
